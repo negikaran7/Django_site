7e0c1f444c3b4d3a5c003e4be860b648d562400e
negikaran7 <knegi3422@gmail.com>
2018-11-01 10:50:28 +0530

    forms

--- django/Django_project/products/forms.py
+++ django/Django_project/products/forms.py
@@ -9,3 +9,14 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
     ]
+
+class RawProductForm(forms.Form):
+    title=forms.CharField(label='',widget=forms.TextInput(attrs={"placeholder":"your title"}))
+    description=forms.CharField(required=False,widget=forms.Textarea(attrs={
+        "class":"new-class-name two",
+        "rows":2,
+        "cols":20,
+        "placeholder":"your description"
+
+    }))
+    price=forms.FloatField(initial=99.99)
