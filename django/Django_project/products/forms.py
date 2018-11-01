e7bf9d6d17b98f2d362d36c9ff121b354b80306a
negikaran7 <knegi3422@gmail.com>
2018-11-01 11:19:23 +0530

    validations

--- django/Django_project/products/forms.py
+++ django/Django_project/products/forms.py
@@ -2,6 +2,16 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title=forms.CharField(label='',widget=forms.TextInput(attrs={"placeholder":"your title"}))
+    email=forms.EmailField()
+    description=forms.CharField(required=False,widget=forms.Textarea(attrs={
+        "class":"new-class-name two",
+        "rows":2,
+        "cols":20,
+        "placeholder":"your description"
+
+    }))
+    price=forms.FloatField(initial=99.99)
     class Meta:
         model=Product
         fields=[
@@ -9,6 +19,19 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
     ]
+
+    def clean_title(self,*args,**kwargs):
+        title=self.cleaned_data.get("title")
+        if "karan" in title:
+            return title
+        else:
+            raise forms.ValidationError("this is not a valid title")
+
+    def clean_email(self,*args,**kwargs):
+        email=self.cleaned_data.get("email")
+        if not email.endswith("com"):
+            raise forms.ValidationError("this is not a valid email")
+        return email
 
 class RawProductForm(forms.Form):
     title=forms.CharField(label='',widget=forms.TextInput(attrs={"placeholder":"your title"}))
