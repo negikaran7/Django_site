5096b11770e680c1c99b7d428ea58a6c6ba406ad
negikaran7 <knegi3422@gmail.com>
2018-11-18 13:01:07 +0530

    :sunglasses:

---
+++ codingwars/WeIrD StRiNg CaSe.py
@@ -0,0 +1,10 @@
+def to_weird_case(s):
+    s = list(s)
+    s_even = list(letter.upper() for letter in s[::2])
+    s[::2] = s_even
+    weird = ''
+    for char in s:
+        weird += char
+    return weird
+
+print(to_weird_case("testing weird string case"))
