08943c7ac6ae93eb59f29119b8d585e188a67921
negikaran7 <knegi3422@gmail.com>
2018-11-19 09:29:33 +0530

    templates

--- Flask/mYblog/flaskblog.py
+++ Flask/mYblog/flaskblog.py
@@ -1,14 +1,32 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template,url_for
+app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': "karan",
+        "title": "blog post 1",
+        "date_posted": "Nov 19, 2018",
+        "content":"hello there! it's my first post"
+    },
+    {
+        'author': "karan",
+        "title": "blog post 1",
+        "date_posted": "Nov 19, 2018",
+        "content":"hello there! it's my second post "
+    }
+]
+
 
 @app.route("/")
 def hello():
-    return "<h1>your homepage</h1>"
+    return render_template('home.html',posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h2>this is about page</h2>"
+    return render_template('about.html',title="about")
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
