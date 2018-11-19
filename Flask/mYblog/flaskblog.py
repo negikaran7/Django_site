b4b4699a820dbca90a6e46032f12af5366ebde7a
negikaran7 <knegi3422@gmail.com>
2018-11-19 11:33:45 +0530

    routes & templates

--- Flask/mYblog/flaskblog.py
+++ Flask/mYblog/flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template,url_for
-app = Flask(__name__)
+from flask import Flask, render_template,url_for,flash,redirect
+from forms import RegistrationForm, LoginForm
 
+app = Flask(__name__)
+app.config['SECRET_KEY']='1d8700f8a3a5e0575ed1c5a7b802981'
 
 posts = [
     {
@@ -26,6 +28,20 @@ def hello():
 @app.route("/about")
 def about():
     return render_template('about.html',title="about")
+
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+@app.route("/login")
+def login():
+    form=LoginForm()
+    return render_template('login.html',title='Login',form=form)
 
 
 if __name__ == '__main__':
