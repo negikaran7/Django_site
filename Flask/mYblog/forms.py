c71efe18341509ecac660cf3d641c4a000ba4b42
negikaran7 <knegi3422@gmail.com>
2018-11-19 11:10:38 +0530

    forms

--- Flask/mYblog/forms.py
+++ Flask/mYblog/forms.py
@@ -15,4 +15,5 @@ class LoginForm(FlaskForm):
     email=StringField('Email', validators=[DataRequired(),Email()])
     password=PasswordField('Password', validators=[DataRequired()])
     remember=BooleanField('Remember Me')
-    submit=SubmitField('Sign up')
+    submit=SubmitField('Login')
+    
