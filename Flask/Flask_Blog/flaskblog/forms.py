8a29794468f15bbc1c0e8dde245760ab50a1af8f
negikaran7 <knegi3422@gmail.com>
2018-11-26 06:35:55 +0530

    User login

--- Flask/Flask_Blog/flaskblog/forms.py
+++ Flask/Flask_Blog/flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length,Email,EqualTo
+from wtforms.validators import DataRequired, Length,Email,EqualTo, ValidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -9,6 +10,17 @@ class RegistrationForm(FlaskForm):
     password=PasswordField('Password', validators=[DataRequired()])
     confirm_password=PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
     submit=SubmitField('Sign up')
+
+    def validate_username(self,username):
+        user=User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('username already taken !')
+
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('email already taken !')
+
 
 
 class LoginForm(FlaskForm):
--- Flask/Flask_Blog/flaskblog/routes.py
+++ Flask/Flask_Blog/flaskblog/routes.py
@@ -1,6 +1,5 @@
-
 from flask import render_template, url_for, flash, redirect
-from flaskblog import app
+from flaskblog import app, bcrypt,db
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 
@@ -36,7 +35,11 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
+        hashed_pw=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user=User(username=form.username.data,email=form.email.data,password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'{form.username.data} your account has been created, Login !', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
