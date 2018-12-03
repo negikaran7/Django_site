a0377fe5542c2b801f1ed248b2a20634f66d2757
negikaran7 <knegi3422@gmail.com>
2018-12-03 08:15:58 +0530

    redirect_

--- Flask/Flask_Blog/flaskblog/__init__.py
+++ Flask/Flask_Blog/flaskblog/__init__.py
@@ -9,8 +9,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 db = SQLAlchemy(app)
 
+bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
 login_manager.login_view='login'
-bcrypt=Bcrypt(app)
+login_manager.login_message_category='info'
+
 
 from flaskblog import routes
