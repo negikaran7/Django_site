f86bb474abd907468606a8e44cc180bdd64725ac
negikaran7 <knegi3422@gmail.com>
2018-12-06 10:57:38 +0530

    Mexican Wave

---
+++ codingwars/Mexican_wave.py
@@ -0,0 +1,10 @@
+def wave(str):
+    # Code here
+    new_wave = []
+    for i in range(0, len(str)):
+        if str[i]==" ":
+            pass
+        else:
+            new_wave.append(str[0:i]+str[i].upper()+str[i+1:])
+    return new_wave
+print(wave("hello"))
