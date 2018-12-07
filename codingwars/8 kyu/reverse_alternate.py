023106e9416d287fecb15d8319cd4eb773a2c58f
negikaran7 <knegi3422@gmail.com>
2018-12-07 10:25:52 +0530

    WeIrD StRiNg CaSe

--- codingwars/8 kyu/reverse_alternate.py
+++
@@ -1,9 +0,0 @@
-def reverse_alternate(string):
-    words=string.split(" ")
-    new_words=[]
-    for i in range(len(words)):
-        w=words[i]
-        new_words.append(w) if i==0 or i%2==0 else new_words.append(w[::-1])
-    return ' '.join(map(str, new_words))
-
-print(reverse_alternate("karan singh negi hello how are you"))
