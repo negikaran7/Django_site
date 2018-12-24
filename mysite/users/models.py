6f74084ae31e83551d302b206cec2c750cb8bf69
negikaran7 <knegi3422@gmail.com>
2018-12-24 15:08:23 +0530

    updates

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -13,8 +13,8 @@ class Profile(models.Model):
     def save(self,**kwargs):
         super().save()
 
-        # img = Image.open(self.image.path)
-        # if img.height > 300 or img.width > 300:
-        #     output_size = (300, 300)
-        #     img.thumbnail(output_size)
-        #     img.save(self.image.path)
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
