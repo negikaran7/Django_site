a74521572eb4929d8419605e5b0869802e97e123
negikaran7 <knegi3422@gmail.com>
2019-02-18 09:29:35 +0530

    changes

--- django/mysite/main/models.py
+++ django/mysite/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from django.utils import timezone
 
 class TutorialCategory(models.Model):
     tutorial_category=models.CharField(max_length=200)
@@ -28,7 +29,7 @@ class TutorialSeries(models.Model):
 class Tutorial(models.Model):
     tutorial_title=models.CharField(max_length=200)
     tutorial_content=models.TextField()
-    tutorial_published=models.DateTimeField("date published",default=datetime.now())
+    tutorial_published=models.DateTimeField("date published",default=timezone.now)
     tutorial_series=models.ForeignKey(TutorialSeries,default=1,verbose_name="Series",on_delete=models.SET_DEFAULT)
     tutorial_slug=models.CharField(max_length=200,default=1)
 
--- django/mysite/main/views.py
+++ django/mysite/main/views.py
@@ -8,12 +8,13 @@ from .forms import NewUserForm
 
 
 def single_slug(request, single_slug):
+
     categories = [c.category_slug for c in TutorialCategory.objects.all()]
     if single_slug in categories:
         matching_series = TutorialSeries.objects.filter(
             tutorial_category__category_slug=single_slug)
-
         series_urls = {}
+
         for m in matching_series.all():
             part_one = Tutorial.objects.filter(
                 tutorial_series__tutorial_series=m.tutorial_series).earliest("tutorial_published")
@@ -23,11 +24,11 @@ def single_slug(request, single_slug):
                       template_name='main/category.html',
                       context={"tutorial_series": matching_series, "part_ones": series_urls})
 
-    tutorials = [t.category_slug for t in Tutorial.objects.all()]
+    tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
     if single_slug in tutorials:
-        return HttpResponse(f"{single_slug} is a tutorial!!!")
-
-    return HttpResponse(f"{single_slug} does not respond to anything.")
+        return HttpResponse(f"{single_slug} is a Tutorial")
+        
+    return HttpResponse(f"'{single_slug}' does not correspond to anything we know of!")
 
 
 def homepage(request):
