abc0ea97d77dae702714e0aeb78d4bd64ef68bac
negikaran7 <knegi3422@gmail.com>
2019-02-19 09:19:15 +0530

    deleted

--- django/mysite/mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-
-
-urlpatterns = [
-    path('', include('main.urls')),
-    path('tinymce/',include('tinymce.urls')),
-    path('admin/', admin.site.urls),
-]
