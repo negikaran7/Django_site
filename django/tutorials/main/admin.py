65d2adbf6dc935df069c6f0060fa7c53691b5a60
negikaran7 <knegi3422@gmail.com>
2019-02-19 13:11:42 +0530

    admin setup

--- django/tutorials/main/admin.py
+++ django/tutorials/main/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Tutorial
 
 # Register your models here.
-admin.site.register(Tutorial)
+class TutorialAdmin(admin.ModelAdmin):
+    # fields=[
+    #     "tutorial_title",
+    #     "tutorial_published",
+    #     "tutorial_content"
+    # ]
+    fieldsets=[
+        ("Title",{"fields":["tutorial_title"]}),
+        ("Date_Time",{"fields":["tutorial_published"]}),
+        ("Content",{"fields":["tutorial_content"]})
+    ]
+admin.site.register(Tutorial,TutorialAdmin)
