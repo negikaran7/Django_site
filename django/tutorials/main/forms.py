16987b4ca41a159eff4fbff520a321ab93cf45c1
negikaran7 <knegi3422@gmail.com>
2019-02-20 02:17:23 +0530

    forms

--- django/tutorials/main/forms.py
+++ django/tutorials/main/forms.py
@@ -8,7 +8,11 @@ class NewUserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = (
+            "email",
+            "username",
+            "password1",
+            "password2")
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
