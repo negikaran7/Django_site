a245795f89d6fc6c41cf82804445901797f06d29
negikaran7 <knegi3422@gmail.com>
2019-02-19 16:00:32 +0530

    register form

--- django/tutorials/main/views.py
+++ django/tutorials/main/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Tutorial
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
 def homepage(request):
-    return render(request,"main/home.html",{"tutorials":Tutorial.objects.all})
+    return render(request, "main/home.html", {"tutorials": Tutorial.objects.all})
+
+def register(request):
+    form = UserCreationForm
+    return render(request,"main/register.html",{"form":form})
