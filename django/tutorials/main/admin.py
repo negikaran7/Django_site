e5be508b109fc37fbe12e6bfa3574e71c0ff0fb1
negikaran7 <knegi3422@gmail.com>
2019-02-20 20:09:15 +0530

    categories and series

--- django/tutorials/main/admin.py
+++ django/tutorials/main/admin.py
@@ -1,22 +1,29 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialCategory, TutorialSeries
 from tinymce.widgets import TinyMCE
 from django.db import models
 
 # Register your models here.
+
+
 class TutorialAdmin(admin.ModelAdmin):
     # fields=[
     #     "tutorial_title",
     #     "tutorial_published",
     #     "tutorial_content"
     # ]
-    fieldsets=[
-        ("Title",{"fields":["tutorial_title"]}),
-        ("Date&Time",{"fields":["tutorial_published"]}),
-        ("Content",{"fields":["tutorial_content"]})
+    fieldsets = [
+        ("Title", {"fields": ["tutorial_title"]}),
+        ("Date&Time", {"fields": ["tutorial_published"]}),
+        ("URL", {'fields': ["tutorial_slug"]}),
+        ("Series", {'fields': ["tutorial_series"]}),
+        ("Content", {"fields": ["tutorial_content"]})
     ]
-    formfield_overrides={
-        models.TextField:{'widget':TinyMCE()}
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()}
     }
 
-admin.site.register(Tutorial,TutorialAdmin)
+
+admin.site.register(Tutorial, TutorialAdmin)
+admin.site.register(TutorialCategory)
+admin.site.register(TutorialSeries)
