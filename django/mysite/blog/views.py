1a09f668152c18bd716278f82e92386905744c3c
negikaran7 <knegi3422@gmail.com>
2019-02-25 09:48:26 +0530

    posts

--- django/mysite/blog/views.py
+++ django/mysite/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts=[
+    {
+        'author':'karan',
+        'title':'Blog post 1',
+        'content':'first post content',
+        'date_posted':'Feb 25, 2019'
+    },
+    {
+        'author':'optimus',
+        'title':'Blog post 2 ',
+        'content':'second post content',
+        'date_posted':'Feb 25, 2019'
+    }
+]
+
 def home(request):
-    return render(request,'blog/home.html')
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
     return render(request,'blog/about.html')
