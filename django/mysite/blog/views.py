9b0e61f36297fea93f71900d801b4672625bdc86
negikaran7 <knegi3422@gmail.com>
2019-02-25 09:27:00 +0530

    view&urls

--- django/mysite/blog/views.py
+++ django/mysite/blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>welcome to homepage</h1>")
+    return HttpResponse("<h1>welcome to blog homepage</h1>")
+
+def about(request):
+    return HttpResponse("<h1>welcome to blog aboutpage</h1>")
