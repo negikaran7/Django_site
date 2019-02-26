74abb7d7265b8d7860610a0a1f220492c56c4a8c
negikaran7 <knegi3422@gmail.com>
2019-02-26 10:21:50 +0530

    TinyMCE

--- django/mysite/blog/admin.py
+++ django/mysite/blog/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
 from .models import Post
+from tinymce.widgets import TinyMCE
+from django.db import models
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    # fields=[
+    #     "title",
+    #     "date_posted",
+    #     "content",
+    #   "author"
+    # ]
+    fieldsets = [
+        ("Title", {"fields": ["title"]}),
+        ("Date&Time", {"fields": ["date_posted"]}),
+        ("Content", {"fields": ["content"]}),
+        ("author", {"fields": ["author"]})
+    ]
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()}
+    }
+
+
+admin.site.register(Post,PostAdmin)
