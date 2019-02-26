b9d4e952ac26509347bad7bc94adac045af64288
negikaran7 <knegi3422@gmail.com>
2019-02-26 08:57:17 +0530

    Posts from Post model

--- django/mysite/blog/views.py
+++ django/mysite/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts=[
-    {
-        'author':'karan',
-        'title':'Blog post 1',
-        'content':'first post content',
-        'date_posted':'Feb 25, 2019'
-    },
-    {
-        'author':'optimus',
-        'title':'Blog post 2 ',
-        'content':'second post content',
-        'date_posted':'Feb 25, 2019'
-    }
-]
+from .models import Post
 
 def home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
