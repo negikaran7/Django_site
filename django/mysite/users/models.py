7344934e022da6b3d15ab7111213a52b54fd7457
negikaran7 <knegi3422@gmail.com>
2019-02-26 21:22:27 +0530

    profile image

--- django/mysite/users/models.py
+++ django/mysite/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
@@ -9,3 +10,10 @@ class Profile(models.Model):
         return f'{self.user} profile'
 
 
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 400 or img.width > 400:
+            output_size=(400,400)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
