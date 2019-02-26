c723c3f2c2b3ad3df2725be5a998f45833f42528
negikaran7 <knegi3422@gmail.com>
2019-02-26 20:59:26 +0530

    profile

--- django/mysite/users/forms.py
+++ django/mysite/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class NewUserForm(UserCreationForm):
@@ -14,9 +15,14 @@ class NewUserForm(UserCreationForm):
             "password1",
             "password2")
 
-    def save(self, commit=True):
-        user = super(NewUserForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model=User
+        fields=['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model= Profile
+        fields=['image']
