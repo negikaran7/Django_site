b5c378e320c9ce37615b46e54ae0a72d040fa9a2
negikaran7 <knegi3422@gmail.com>
2019-02-26 17:45:07 +0530

    urls

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from users import views as user_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include("blog.urls")),
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
+    path('register/', user_view.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
