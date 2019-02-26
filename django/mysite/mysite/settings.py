13288aa10c2896a496b7e2c4d7a4cf54ab694885
negikaran7 <knegi3422@gmail.com>
2019-02-26 10:02:14 +0530

    Sqlite3 -> Postgresql

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -72,9 +72,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mysite',
+        'USER': 'optimus',
+        'PASSWORD': 'Optimus@7#',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
