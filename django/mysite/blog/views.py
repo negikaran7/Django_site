58782141ad56641d05e91526b6f5667ff05d6499
negikaran7 <knegi3422@gmail.com>
2019-02-26 22:54:42 +0530

    posts

--- django/mysite/blog/views.py
+++ django/mysite/blog/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView,DetailView,CreateView
 
-def home(request):
-    context={
-        'posts':Post.objects.all()
-    }
-    return render(request,'blog/home.html',context)
+# def home(request):
+#     context={
+#         'posts':Post.objects.all()
+#     }
+#     return render(request,'blog/home.html',context)
+
+class PostListView(ListView):
+    model= Post
+    template_name='blog/home.html'
+    context_object_name='posts'
+    ordering=['-date_posted']
+
+class PostDetailView(ListView):
+    model= Post
+
+class PostCreateView(CreateView):
+    model= Post
+    fields=['title','content']
+
+
 
 def about(request):
     return render(request,'blog/about.html',{'title':'about'})
