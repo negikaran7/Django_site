c7a2dc1295d1dc25e54dbb6f3d3c9a8359b062ad
negikaran7 <knegi3422@gmail.com>
2019-02-26 17:16:30 +0530

    crispy_forms

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'tinymce',
 ]
 
@@ -154,3 +155,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
