5c2dcbe2b7fd38e378fb5f0fbb6affb66e9ce66f
negikaran7 <knegi3422@gmail.com>
2019-02-26 17:09:24 +0530

    forms

--- django/mysite/users/views.py
+++ django/mysite/users/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import NewUserForm
 
 def register(request):
     if request.method=="POST":
-        form = UserCreationForm(request.POST)
+        form = NewUserForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'new account created for {username}')
             return redirect("blog-home")
     else:
-        form = UserCreationForm()
+        form = NewUserForm()
     return render(request,'users/register.html',{"form":form})
