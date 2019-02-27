a141f525fea09bb6487b1536c5c7663757e9b271
negikaran7 <knegi3422@gmail.com>
2019-02-27 09:02:28 +0530

    CRUD posts

--- django/mysite/blog/views.py
+++ django/mysite/blog/views.py
@@ -1,27 +1,59 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-# def home(request):
-#     context={
-#         'posts':Post.objects.all()
-#     }
-#     return render(request,'blog/home.html',context)
 
 class PostListView(ListView):
-    model= Post
-    template_name='blog/home.html'
-    context_object_name='posts'
-    ordering=['-date_posted']
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
-class PostDetailView(ListView):
-    model= Post
 
-class PostCreateView(CreateView):
-    model= Post
-    fields=['title','content']
+class PostDetailView(DetailView):
+    model = Post
 
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = Post
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
-    return render(request,'blog/about.html',{'title':'about'})
+    return render(request, 'blog/about.html', {'title': 'about'})
