5322ab73d920f6a2d8d01468201ba29facd88872
negikaran7 <knegi3422@gmail.com>
2019-03-02 15:31:37 +0530

    corrections

--- django/mysite/users/models.py
+++ django/mysite/users/models.py
@@ -2,18 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg',upload_to='profile_pics')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user} profile'
 
-
-    def save(self):
-        super().save()
+    def save(self,*args,**kwargs):
+        super(Profile, self).save(*args, **kwargs)
         img = Image.open(self.image.path)
         if img.height > 400 or img.width > 400:
-            output_size=(400,400)
+            output_size = (400, 400)
             img.thumbnail(output_size)
-            img.save(self.image.path)
+            img.save(self)
