90ed204312a569561100261e7149397859a6269b
negikaran7 <knegi3422@gmail.com>
2019-06-25 07:47:27 +0530

    format

--- django/newproject/newproject/core/views.py
+++ django/newproject/newproject/core/views.py
@@ -1,23 +1,25 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
+
 def home(request):
-    count=User.objects.count()
-    return render(request,'home.html',{'count':count})
+    count = User.objects.count()
+    return render(request, 'home.html', {'count': count})
+
 
 def signup(request):
-    if request.method=='POST':
-        form=UserCreationForm(request.POST)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
     else:
-        form=UserCreationForm()
-    return render(request,'registration/signup.html',{'form':form})
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 # @login_required
 # def secret_page(request):
